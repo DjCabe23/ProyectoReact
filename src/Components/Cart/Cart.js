0087a864895e88c5456f2944d335405c71237a6c
DjCabe23 <damianramirez343@gmail.com>
2023-06-11 17:04:23 -0300

    ProyectoReact

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import Cart, { FaShoppingCart } from "react-icons/fa";
+
+function Carto() {
+    return (
+        <div>
+            <FaShoppingCart />
+            0
+        </div>
+    )
+}
+
+
+export default Carto
---
+++ src/Components/NavBar.js
@@ -0,0 +1,34 @@
+import "bulma/css/bulma.css"
+import Carito from "./Cart/Cart"
+
+function NavBar() {
+    return(
+        <nar>
+             <div className="container is-max-desktop has-text-centered">
+                 <div className="notification is-primary" >
+                    <strong>LO DEL CABE</strong> 
+                    <Carito/>
+                 </div>
+            </div>
+
+            <div className="columns is-centered">
+                <div className="column is-narrow">
+                    <button class="button is-large">Home</button>
+                    </div>
+                    <div className="column is-narrow">
+                    <button class="button is-large is-multiline" >Tienda</button>
+                    </div>
+                    <div className="column is-narrow">
+                    <button class="button is-large is-multiline">Nosotros</button>
+                    </div>
+
+      
+            </div>
+      
+        </nar>
+    )
+
+    
+}
+
+export default NavBar
